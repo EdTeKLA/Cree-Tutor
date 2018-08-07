1bf498052281abde85624a79bcb345fd771a7979
AnakaSparrow <asparrow@ualberta.ca>
2018-08-07 13:03:41 -0600

    added log file that sends and receives data but doesn't change anything, made populating the invaders a funciton

---
+++ CreeTutorBackEnd/lettergame/static/lettergame/js/invaders_log.js
@@ -0,0 +1,45 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+function postInvaders(shot, remains, more=false){
+    // e.preventDefault();
+    $.ajax({
+      type:'POST',
+      url: "",
+      data:{
+        shot: 'shot',
+        remains: 'remains',
+        more: more
+      },
+      success:function(data){
+        console.log(data);
+      },
+      error:function(error){console.log(error)}
+    });
+}
