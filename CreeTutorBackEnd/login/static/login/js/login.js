195a08fde3247cc437e3196274f852974fa53156
sehoven <shoven@ualberta.ca>
2018-10-05 16:41:10 -0600

    Improved Login UI.

--- CreeTutorBackEnd/login/static/login/js/login.js
+++ CreeTutorBackEnd/login/static/login/js/login.js
@@ -1,26 +1,44 @@
 $(function() {
 
-    $('#login-form').click(function(e) {
+    $('#login-form-tab').click(function(e) {
         $("#login-form")
-            .delay(100)
-            .fadeIn(100)
-            .addClass('active');
+            .delay(200)
+            .fadeIn(200);
         $("#signup-form")
-            .fadeOut(100)
-            .removeClass('active');
+            .fadeOut(200);
+        $(this).addClass('active');
+        $("#signup-form-tab").removeClass('active');
         e.preventDefault();
     });
-    $('#signup-form').click(function(e) {
-        $("#signup-form")
-            .delay(100)
-            .fadeIn(100);
-        $("#login-form")
-            .fadeOut(100)
-            .removeClass('active');
 
+    $('#signup-form-tab').click(function(e) {
+        $("#signup-form")
+            .delay(200)
+            .fadeIn(200);
+        $("#login-form")
+            .fadeOut(200);
         $(this).addClass('active');
-        console.log($(this));
+        $("#login-form-tab").removeClass('active');
         e.preventDefault();
+    });
+
+    $('#password').on('input', function() {
+        const confirmPassword = $('#confirm-password');
+        if(confirmPassword.val().length > 0 && $(this).val() !== confirmPassword.val()) {
+            confirmPassword.addClass('mismatch');
+        } else if (confirmPassword.hasClass('mismatch')) {
+            confirmPassword.removeClass('mismatch');
+        }
+    });
+
+    $('#confirm-password').on('input', function() {
+        const confirmPassword = $(this);
+        const password = $('#password');
+        if(password.val().length > 0 && password.val() !== confirmPassword.val()) {
+            confirmPassword.addClass('mismatch');
+        } else if (confirmPassword.hasClass('mismatch')) {
+            confirmPassword.removeClass('mismatch');
+        }
     });
 
 });
