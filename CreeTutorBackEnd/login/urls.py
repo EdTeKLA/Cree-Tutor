d6f2c05f2a68a5ecb76146b43cdc64cd27ea1611
sehoven <shoven@ualberta.ca>
2018-10-05 14:48:14 -0600

    Added new blank app for login

---
+++ CreeTutorBackEnd/login/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'login'
+urlpatterns = [
+    # path('index/', views.index, name='index'),
+    # path('signin/', views.signin, name='signin'),
+    # path('welcome/', views.welcome, name='welcome'),
+    # path('signout/', views.signout, name='signout'),
+    # path('account/create/', views.create, name='create')
+]
