6351e3c40e7267ccbc28218983424299721b2fa1
Delaney <dlothian@ualberta.ca>
2018-10-08 19:28:02 -0600

    user can create an account, be denied login if email/password are wrong, and can succesfully login

--- CreeTutorBackEnd/login/urls.py
+++ CreeTutorBackEnd/login/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'login'
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    # path('logout/', views.logout, name='logout'),
-    # path('account/create/', views.create, name='create')
+    path('login/', views.index, name='index'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('signup/', views.create, name='create')
 ]
--- CreeTutorBackEnd/login/views.py
+++ CreeTutorBackEnd/login/views.py
@@ -4,16 +4,74 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.db import models
 
-def login(request):
-    if request.method == "POST":
-        if request.POST.get("submit") == "log_in":
-            # your sign in logic goes here
-            print("LOG IN")
-            pass
-        elif request.POST.get("submit") == "sign_up":
-            # your sign up logic goes here
-            print("SIGN UP")
-            pass
 
-    return render(request, "login/index.html")
+def uniqueEmail(email):
+    try:
+        u = User.objects.get(username=email)
+    except User.DoesNotExist:
+        u = None
 
+    if u != None:
+        return False
+    else:
+        return True
+
+def checkEmail(email):
+    error=None
+    if not uniqueEmail(email):
+        error = "That email is already taken."
+    elif '@' not in email:
+        error = "That is not a valid email address."
+    elif len(email) < 6:
+        error = "That is not a long enough email"
+    return error
+
+def index(request):
+
+    return render(request, 'login/index.html')
+
+def signin(request):
+    try:
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('lettergame:index')
+        else:
+            context={'error':"Email or password is incorrect.", 'email':email, 'password':password}
+            return render(request, 'login/index.html', context)
+
+    except KeyError:
+        return HttpResponse('ERROR: ' + str(KeyError))
+
+
+def signout(request):
+    logout(request)
+    return redirect('login:index')
+
+def create(request):
+    try:
+        email = request.POST['email']
+        password = request.POST['password']
+
+        error = checkEmail(email)
+
+        if error == None:
+            # Create User object
+            user = User.objects.create_user(email, email, password)
+
+            # Log user in
+            login(request,user)
+
+            user.save()
+            return redirect('lettergame:index')
+
+        else:
+            context={'email':email, 'password':password, 'error':error}
+            return(render(request, 'login/index.html', context))
+
+    except KeyError:
+        return render(request, 'login/index.html')
