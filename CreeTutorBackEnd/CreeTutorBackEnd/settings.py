f618ffa5bd5a865b1906d2eec4230c90cc4b0f61
Ryan Perez <rperez@ualberta.ca>
2018-11-19 15:40:34 -0700

    reverted a change that's in discussion

--- CreeTutorBackEnd/CreeTutorBackEnd/settings.py
+++ CreeTutorBackEnd/CreeTutorBackEnd/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
