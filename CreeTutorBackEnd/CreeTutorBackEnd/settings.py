28f9e7f53d14b676c3a0cd5d47d648a93f41f1ae
ry-prz <rperez@ualberta.ca>
2018-11-21 14:42:20 -0700

    Update CreeTutorBackEnd/CreeTutorBackEnd/settings.py
    Co-Authored-By: sehoven <sehoven@users.noreply.github.com>

--- CreeTutorBackEnd/CreeTutorBackEnd/settings.py
+++ CreeTutorBackEnd/CreeTutorBackEnd/settings.py
@@ -132,7 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = 'static'
+# Where the webserver will collect all static files into a single directory
+# for deployment
+# https://docs.djangoproject.com/en/2.1/howto/static-files/#deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
