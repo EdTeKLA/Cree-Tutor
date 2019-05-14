3588db19eb2d0e2b6b4f53b92de1910010cc104b
Amrinder Grewal <asgrewal@ualberta.ca>
2019-05-14 10:37:35 -0600

    Small changes to url routing.

--- CreeTutorBackEnd/home/views.py
+++ CreeTutorBackEnd/home/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
 from lettergame.view_helper import *
 from time import gmtime, strftime
 from django.http import HttpResponse
 
 # Create your views here.
-class Home(View):
+class Home(RedirectView):
     """
     Class was created to render and return the main page for games.
     """
-    template_name = 'home/index.html'
+    pattern_name = 'login:index'
