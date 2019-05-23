f66b338d976b2b7dcc6b8d6b8a18a29e871cdb78
Amrinder Grewal <asgrewal@ualberta.ca>
2019-05-23 10:47:11 -0600

    Lettergame/pairs now displays as a single speaker with buttons as options. No feedback yet like before. Will be added later.

--- CreeTutorBackEnd/lettergame/static/lettergame/js/logging.js
+++ CreeTutorBackEnd/lettergame/static/lettergame/js/logging.js
@@ -5,44 +5,67 @@ var startHover = undefined;
 var endHover = undefined;
 var hovered = 'not changed';
 var distractors;
+var audio = undefined;
 
 $(window).on("load", function(e){
     // Onload, function sets StartTime to when the user started looking at this page
     startTime = getTime();
+    // Show the tooltip that suggests that the person click the speaker button to play the audio for 1500ms
+    $('[data-toggle="tooltip"]').tooltip();
+    $("#speaker-button").tooltip('show');
+    setTimeout(function(){ $("#speaker-button").tooltip('hide'); }, 1500);
+    // Get the audio file and click the speaker
+    getAudioAndClickSpeaker();
+
     return;
 });
+
+function getAudioAndClickSpeaker(){
+    // Get the audio file from the server
+    audio = new Audio(audioFileLocation, playAudio);
+    // Click the speaker button to play the audio
+    $("#speaker-button").click();
+}
+
+$(document).on('click', '#speaker-button', function (e) {
+    // When the speaker-button is clicked, the function that plays audio is called
+    playAudio();
+});
+
+function playAudio(){
+    // Function plays the audio
+    audio.play();
+}
 
 function getTime(){
     // Function gets current date and converts it into ISO format
     // returns the date
     wT = new Date();
-    // console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
     whichTime = wT.toISOString();
-    // console.log(whichTime);
     return whichTime;
 }
 
 
 function getDistractors(){
-    // gets all the values of the radio buttons, except for the correct answer, and pushes them onto a global list
+    // gets all the values of the submit buttons, except for the correct answer, and pushes them onto a global list
     distractors = [];
-    let letters = $('input[type=radio]').each(function(){$(this).attr('value')});
+    let letters = $('[type=submit]').each(function () { $(this).attr('value'); });
     let correct = $('#correct_r').attr('value');
     for(let i = 0; i < letters.length; i++){
         if(letters[i].value != correct){
             distractors.push(letters[i].value);
         }
     }
-    // console.log(distractors);
     return;
 }
 
 
-$('body').on('mouseenter', 'input[type=radio]', function(){
+$(document).on('mouseenter', '[type=submit]', function(){
     // Gets the time for when user hovers over a radio input
     startHover = getTime();
 });
-$('body').on('mouseout', 'input[type=radio]', function(){
+
+$(document).on('mouseout', '[type=submit]', function(){
     // Gets the time for when a user leaves hover over a radio input
     hovered = $(this).attr('value');
     endHover = getTime();
@@ -52,16 +75,17 @@ $('body').on('mouseout', 'input[type=radio]', function(){
     return;
 });
 
-$(document).on('submit', '#game_ans', function(e){
-    getDistractors();
+$(document).on('click', '[type=submit]', function(e){
+    // When a button is pushed, send a POST request logging the event and getting a new set up of questions
     e.preventDefault();
+    getDistractors();
     endTime = getTime();
 
     $.ajax({
         type:'POST',
         url: "",
         data:{
-            user_r:$('input[name=user_r]:checked').val(),
+            user_r:$(this).attr('value'),
             correct_r:$("#correct_r").val(),
             time_s: startTime,
             time_e: endTime,
@@ -74,53 +98,33 @@ $(document).on('submit', '#game_ans', function(e){
         },
         error:function(error){console.log(error)}
     });
+
     startTime = getTime();
 });
 
 
 function modifyForm(formID, data){
-    console.log("modifyForm");
     // Update the correct response
     var correct = document.getElementById("correct_r");
     correct.setAttribute("value", data['correct']);
 
     // All of the children of the form in game.html have the class 'choice_button'. Delete them
-    $(".choice_button").remove();
+    $(".submit_answer").remove();
     var form = document.getElementById(formID);
 
-    // Update the src path of the audio file and load the new audio file
-    var audio = document.getElementById("aud");
-    path = '/static/' + data['sound'];
-    var sound = document.getElementById("sound");
-    sound.setAttribute("src", path);
-    audio.load();
+    // Load the new audio file once you get the new links
+    audioFileLocation = '/static/' + data['sound'];
+    getAudioAndClickSpeaker();
 
     // Create the new radio options
     for(var i = 0, l = data['letters'].length; i < l; i++){
         var datum = data['letters'][i];
         var rad = document.createElement("INPUT");
-        rad.setAttribute("type", "radio");
-        rad.setAttribute("class", "choice_button");
-        rad.setAttribute("required", true);
-        rad.setAttribute("name", "user_r");
+        rad.setAttribute("type", "submit");
+        rad.setAttribute("class", "btn btn-default m-3 pl-5 pr-5 submit_answer");
         rad.setAttribute("value", datum);
-        var lab = document.createElement("LABEL");
-        lab.setAttribute("for", datum);
-        lab.setAttribute("class", "choice_button");
-        lab.innerHTML = datum;
-        linebreak = document.createElement("br");
-        linebreak.setAttribute("class", "choice_button");
         form.appendChild(rad);
-        form.appendChild(lab);
-        form.appendChild(linebreak);
     }
-
-    //Re-create the new submit button
-    var sub = document.createElement("INPUT");
-    sub.setAttribute("class","btn btn-primary choice_button");
-    sub.setAttribute("type", "submit");
-    sub.setAttribute("value","submit");
-    form.appendChild(sub);
 
     return;
 }
